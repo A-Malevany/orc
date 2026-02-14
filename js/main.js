cd47ef29c32794e6f94c4fd26d030623d86de752
Alexander Malevany <malevany9@gmail.com>
2026-02-14 16:59:33 +0300

    add main page full

--- js/main.js
+++ js/main.js
@@ -1,3 +1,58 @@
 function toggleSearch() {
 }
 toggleSearch();
+
+function initSwiperPromo() {
+    const slider = document.querySelector('.promo .swiper')
+
+    if (slider) {
+        new Swiper(slider, {
+            loop: true,
+            speed: 600,
+
+            pagination: {
+                el: slider.querySelector('.swiper-pagination')
+            },
+
+            navigation: {
+                nextEl: slider.querySelector('.swiper-button-next'),
+                prevEl: slider.querySelector('.swiper-button-prev')
+            }
+        }) 
+    }
+}
+initSwiperPromo();
+
+function initVideoFront() {
+    const btn = document.querySelector('.video__play');
+    const video = document.querySelector('.video video');
+
+    video.addEventListener('click', () => {
+        btn.classList.toggle('active');
+
+        if ( video.paused ) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    })
+}
+initVideoFront();
+
+function handleFileSelect() {
+    const fileInput = document.querySelector('.calculation-form__file input[type=file]');
+    const nameDisplay = document.querySelector('.calculation-form__file span');
+
+    if ( fileInput && nameDisplay ) {
+        fileInput.addEventListener('change', (e) => {
+            const file = e.target.files[0];
+
+            if (file) {
+                nameDisplay.textContent = file.name;
+            } else {
+                nameDisplay.textContent = 'Прикрепить файл к заявке';
+            }
+        })
+    }
+}
+handleFileSelect();
